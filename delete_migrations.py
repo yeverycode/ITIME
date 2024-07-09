f2f291208b856fdacf0fafa9f01258981b7158ae
yeverycode <ktyjj030677@gmail.com>
2024-07-09 14:58:35 +0900

    Delete delete_migrations.py

--- delete_migrations.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-
-for migration_file in glob.glob('**/migrations/*.py', recursive=True):
-    if os.path.basename(migration_file) != '__init__.py':
-        os.remove(migration_file)
-
-for migration_file in glob.glob('**/migrations/*.pyc', recursive=True):
-    os.remove(migration_file)
