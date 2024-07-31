ede849c7ce0fff5e7195e4eb293dfc61367d5ec5
조예인 <ktyjj030677@gmail.com>
2024-07-31 11:49:41 +0900

    7/31 이미지 누끼 수정

--- lecture/views.py
+++ lecture/views.py
@@ -86,3 +86,42 @@ def submit_review(request):
 
         return redirect('lecture_detail', lecture_id=lecture.id)
     return redirect('lecture_room')
+
+from django.shortcuts import render, get_object_or_404
+from .models import Lecture, Review
+
+def lecture_detail(request, lecture_id):
+    lecture = get_object_or_404(Lecture, pk=lecture_id)
+    reviews = Review.objects.filter(lecture=lecture)
+
+    total_reviews = reviews.count()
+
+    def calculate_percentage(count):
+        return (count / total_reviews) * 100 if total_reviews > 0 else 0
+
+    context = {
+        'lecture': lecture,
+        'total_reviews': total_reviews,
+        'no_homework_percentage': calculate_percentage(reviews.filter(homework='없음').count()),
+        'average_homework_percentage': calculate_percentage(reviews.filter(homework='보통').count()),
+        'many_homework_percentage': calculate_percentage(reviews.filter(homework='많음').count()),
+        'no_groupwork_percentage': calculate_percentage(reviews.filter(groupwork='없음').count()),
+        'average_groupwork_percentage': calculate_percentage(reviews.filter(groupwork='보통').count()),
+        'many_groupwork_percentage': calculate_percentage(reviews.filter(groupwork='많음').count()),
+        'generous_grading_percentage': calculate_percentage(reviews.filter(grading='너그러움').count()),
+        'average_grading_percentage': calculate_percentage(reviews.filter(grading='보통').count()),
+        'strict_grading_percentage': calculate_percentage(reviews.filter(grading='깐깐함').count()),
+        'complex_attendance_percentage': calculate_percentage(reviews.filter(attendance='복합적').count()),
+        'direct_attendance_percentage': calculate_percentage(reviews.filter(attendance='직접호명').count()),
+        'designated_seating_percentage': calculate_percentage(reviews.filter(attendance='지정좌석').count()),
+        'electronic_attendance_percentage': calculate_percentage(reviews.filter(attendance='전자출결').count()),
+        'no_attendance_percentage': calculate_percentage(reviews.filter(attendance='반영안함').count()),
+        'four_or_more_exams_percentage': calculate_percentage(reviews.filter(exams='네 번 이상').count()),
+        'three_exams_percentage': calculate_percentage(reviews.filter(exams='세 번').count()),
+        'two_exams_percentage': calculate_percentage(reviews.filter(exams='두 번').count()),
+        'one_exam_percentage': calculate_percentage(reviews.filter(exams='한 번').count()),
+        'no_exams_percentage': calculate_percentage(reviews.filter(exams='없음').count()),
+        'reviews': reviews,
+    }
+
+    return render(request, 'lecture/lecture_detail.html', context)
